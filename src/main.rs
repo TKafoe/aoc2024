777598d2636a81f1db33293cf88e54463bcf0cff
Tom Kafoe <35031598+TKafoe@users.noreply.github.com>
2024-12-02 20:03:12 +0100

    Solve day2 (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 mod day1;
+mod day2;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,6 +9,7 @@ fn main() {
 
     match day_index {
         1 => day1::solver::solve(),
+        2 => day2::solver::solve(),
         _ => println!("No day passed. Quitting."),
     }
 }
